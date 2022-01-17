4eb5e6ceefb0df7bd374fb6b5efe498c0c6bf066
Omas Abdullah <omas.abdullah@gmail.com>
2022-01-16 20:18:48 -0500

    Initial Commit

---
+++ PROG8470_Lab1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PROG8470_Lab1
+//
+//  Created by Omas Abdullah on 2022-01-16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
